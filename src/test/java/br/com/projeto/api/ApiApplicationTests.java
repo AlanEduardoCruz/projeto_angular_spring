6fca383269d327951a9967df8243a01e44825350
Alan <alan-hd@hotmail.com>
2024-06-19 21:36:01 -0300

    Projeto criado

---
+++ src/test/java/br/com/projeto/api/ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.projeto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class);
+	
+
+	}
+
+}
---
+++ src/test/java/br/com/projeto/api/controle/Controle.java
@@ -0,0 +1,36 @@
+package br.com.projeto.api.controle;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.modelo.Cliente;
+import br.com.projeto.api.repositorio.Repositorio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class Controle {
+
+    @Autowired
+    private Repositorio acao;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c){
+
+        return acao.save(c);
+
+    }
+
+
+    @GetMapping("/")
+    public Iterable <Cliente> selecionar (){
+        return acao.findAll();
+    }
+    
+   
+    
+}
