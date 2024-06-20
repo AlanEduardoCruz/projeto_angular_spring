3d9adfe377ea4efd35c530b9940d7e70308c83ef
Alan <alan-hd@hotmail.com>
2024-06-19 21:49:35 -0300

    crud realizado

--- src/test/java/br/com/projeto/api/controle/Controle.java
+++ src/test/java/br/com/projeto/api/controle/Controle.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.repositorio.Repositorio;
+import lombok.experimental.Delegate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -20,16 +25,28 @@ public class Controle {
 
     @PostMapping("/")
     public Cliente cadastrar(@RequestBody Cliente c){
-
         return acao.save(c);
-
     }
-
 
     @GetMapping("/")
     public Iterable <Cliente> selecionar (){
         return acao.findAll();
     }
+
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente c){
+        return acao.save(c);
+
+    }
+
+    @DeleteMapping("/{codigo}")
+    public ResponseEntity<String> remover (@PathVariable long  codigo){
+        acao.deleteById(codigo);       
+        return ResponseEntity.ok("Cliente removido com sucesso!");
+
+    }
+
+
     
    
     
