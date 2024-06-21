4a6ee534ef594f5126c8ef62496d6e999e90ccaa
Alan <alan-hd@hotmail.com>
2024-06-21 15:26:13 -0300

    Adicionando anotation

--- src/test/java/br/com/projeto/api/modelo/Cliente.java
+++ src/test/java/br/com/projeto/api/modelo/Cliente.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,10 +22,14 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long codigo;
 
+    @NotBlank(message = "Nome não pode estar em branco")
+    @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
     private String nome;
 
+    @Min(value = 0, message = "Idade deve ser maior ou igual a 0")
     private int idade;
 
+    @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
     private String cidade;
     
 }
